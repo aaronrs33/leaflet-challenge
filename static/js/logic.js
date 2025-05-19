178196e4c584cf50547a266bd800211775ecc53d
aaronrs33 <aaronrs33@gmail.com>
2025-05-18 19:14:03 -0700

    leaflet-challenge completed

---
+++ static/js/logic.js
@@ -0,0 +1,90 @@
+// Create the 'basemap' tile layer that will be the background of our map.
+let basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'OpenStreetMap contributors'
+});
+
+
+// Create the map object with center and zoom options.
+let map = L.map("map", {
+  center: [20,0],
+  zoom: 2,
+  layers: [basemap]
+});
+
+
+// Make a request that retrieves the earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. Pass the magnitude and depth of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the color of the marker based on the depth of the earthquake.
+  function getColor(depth) {
+    return depth > 90 ? "#ea2c2c" :
+          depth > 70 ? "#ea822c" :
+          depth > 50 ? "#ee9c00" :
+          depth > 30 ? "#eecc00" :
+          depth > 10 ? "#d4ee00" :
+                       "#98ee00";
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  function getRadius(magnitude) {
+    return magnitude === 0 ? 1 : magnitude * 4;
+  }
+
+  // Add a GeoJSON layer to the map once the file is loaded.
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: "
+        + feature.properties.mag
+        + "<br>Depth: "
+        + feature.geometry.coordinates[2]
+        + "<br>Location: "
+        + feature.properties.place
+      );
+    }
+  }).addTo(map);
+
+  // Create a legend control object.
+  let legend = L.control({
+    position: "bottomright"
+  });
+
+  // Then add all the details for the legend
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+
+    // Initialize depth intervals and colors for the legend
+    let depths = [-10, 10, 30, 50, 70, 90];
+    let colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
+    // Loop through our depth intervals to generate a label with a colored square for each interval.
+    for (let i = 0; i < depths.length; i++) {
+      div.innerHTML += "<i style = 'background: " + colors[i] + "'></i>"
+        + depths[i] + (depths[i+1] ? "&ndash;" + depths[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  // Finally, add the legend to the map.
+  legend.addTo(map);
+});
+
+
